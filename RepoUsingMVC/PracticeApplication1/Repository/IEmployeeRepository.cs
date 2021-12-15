2ed5d8e5d131649d9b1f44825bf33a37cd7ec162
LZ3OBZ@alsn.com <MY_NAME@example.com>
2021-12-15 17:56:12 +0530

    RepoUsingMVC

---
+++ RepoUsingMVC/PracticeApplication1/Repository/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using PracticeApplication1.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeApplication1.Repository
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int EmployeeID);
+        void Insert(Employee employee);
+        void Update(Employee employee);
+        void Delete(int EmployeeID);
+        void Save();
+    }
+}
