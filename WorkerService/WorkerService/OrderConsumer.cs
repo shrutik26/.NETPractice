7ec5994a62ffe170caf5f23a9789c33c38037cdb
LZ3OBZ@alsn.com <MY_NAME@example.com>
2021-12-22 12:07:43 +0530

    Initial commit

---
+++ WorkerService/WorkerService/OrderConsumer.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkerService
+{
+    public class OrderConsumer : IConsumer<Order>
+    {
+        private readonly ILogger<OrderConsumer> logger;
+
+        public OrderConsumer(ILogger<OrderConsumer> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<Order> context)
+        {
+           await Console.Out.WriteLineAsync(context.Message.Name);
+            logger.LogInformation($"Got new message {context.Message.Name}");
+        }
+    }
+}
