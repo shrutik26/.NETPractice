b3421dbf6cca137e38d89e9217b44829c4b83098
LZ3OBZ@alsn.com <MY_NAME@example.com>
2022-01-05 16:02:14 +0530

    Initial Commit

---
+++ MethodImplementation/MethodImplementation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MethodImplementation
+{
+    public class Customer
+    {
+        private string Name;
+
+        public void setName(string name)
+        {
+            Name = name;
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.setName("Shrutik Deshmukh");
+            Console.WriteLine(customer.getName());
+        }
+    }
+}
