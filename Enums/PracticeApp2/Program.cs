2d656c411f9d9ea4e4175c01f35957439e309c6e
LZ3OBZ@alsn.com <MY_NAME@example.com>
2022-01-05 15:59:25 +0530

    Initial Commit

---
+++ Enums/PracticeApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PracticeApp2
+{
+    public enum collegeDetails
+    {
+        Name = 1,
+        Age = 2,
+        Address = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = collegeDetails.Name;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((collegeDetails)methodId);
+        }
+    }
+}
