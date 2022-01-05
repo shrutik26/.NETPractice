d54474e0e3c146469737d5ea0d6aef1bc084c3c7
LZ3OBZ@alsn.com <MY_NAME@example.com>
2022-01-05 16:03:22 +0530

    Initial Commit

---
+++ StaticClassImplementation/StaticClassImplementation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StaticClassImplementation
+{
+     class MyCollege
+    {
+        public static string CollegeName;
+        public static string Address;
+
+        static MyCollege()
+        {
+            CollegeName = "PRMITR";
+            Address = "Badnera";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyCollege myCollege = new MyCollege();
+            Console.WriteLine(MyCollege.CollegeName);
+            Console.WriteLine(MyCollege.Address);
+            Console.Read();
+        }
+    }
+}
