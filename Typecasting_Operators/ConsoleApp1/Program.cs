03ff47a2ee0836575f61e11cb94b8d433335c493
LZ3OBZ@alsn.com <MY_NAME@example.com>
2022-01-05 16:04:21 +0530

    Initial Commit

---
+++ Typecasting_Operators/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type Conversion
+
+            //byte b = 16;
+            //int i = b;
+            //Console.WriteLine(i);
+
+            //int i = 16;
+            //byte b = (byte)i;
+            //Console.WriteLine(b);
+
+            //var number = "23432";
+            ////int i = (int) number;
+            //int i = Convert.ToInt32(number);
+            //Console.WriteLine(i);
+
+            //var number = "2343";
+            //byte b = Convert.ToByte(number);
+            //Console.WriteLine(b);
+
+
+            //try
+            //{
+            //    var number = "2343";
+            //    byte b = Convert.ToByte(number);
+            //    Console.WriteLine(b);
+            //}
+            //catch (Exception)
+            //{
+
+            //    Console.WriteLine("The No could not be converted to byte");
+            //}
+
+
+            //Operators
+
+            //var a = 17;
+            //var b = 3;
+            //Console.WriteLine((float)a /(float) b);
+
+            //var a = 17;
+            //var b = 3;
+            //Console.WriteLine(a != b);
+
+            //var a = 17;
+            //var b = 3;
+            //var c = 2;
+            //Console.WriteLine(c < b && a < c);
+
+
+            //var a = 17;
+            //var b = 2;
+            //var c = 3;
+            //Console.WriteLine(c > b || b == a);
+
+        }
+    }
+}
