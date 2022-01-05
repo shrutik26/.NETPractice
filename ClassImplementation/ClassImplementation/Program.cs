70a24f79cc8aa5de19275d9e3506db4ddeb1b6e7
LZ3OBZ@alsn.com <MY_NAME@example.com>
2022-01-05 15:57:42 +0530

    Initial Commit

---
+++ ClassImplementation/ClassImplementation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer(1,"Shrutik");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
