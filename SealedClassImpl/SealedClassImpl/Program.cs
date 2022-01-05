0e4e645a01951324755c6d821d7994b88dad1f94
LZ3OBZ@alsn.com <MY_NAME@example.com>
2022-01-05 16:02:58 +0530

    Initial Commit

---
+++ SealedClassImpl/SealedClassImpl/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SealedClassImpl
+{
+    sealed class SealedClass
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+
+    public class Math : SealedClass
+    {
+        public int Width;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SealedClass sealedClass = new SealedClass();
+            int value = sealedClass.Add(10, 20);
+            Console.WriteLine("Total = " + value.ToString());
+        }
+    }
+}
