515cd52687b0f972165a9607468311486aae0739
LZ3OBZ@alsn.com <MY_NAME@example.com>
2022-01-05 15:58:09 +0530

    Initial Commit

---
+++ ControlflowStatements/ControlflowStatements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ControlflowStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 19;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("Its Morning !!");
+            }
+            else if (hour > 12 && hour < 18)
+            {
+                Console.WriteLine("Its AfterNoon !!");
+            }
+            else
+            {
+                Console.WriteLine("Its Evening !!");
+            }
+
+        }
+    }
+}
