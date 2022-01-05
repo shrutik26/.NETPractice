079f3f9d6e7fcc1d7a19f6d3fc4139641789e2da
LZ3OBZ@alsn.com <MY_NAME@example.com>
2022-01-05 16:00:58 +0530

    Initial Commit

---
+++ IterationStatements/IterationStatements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Shrutik Deshmukh";
+            //var number = new int[3] { 1,2,3};
+
+            //for(var i=0; i<name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //foreach(var names in name)
+            //{
+            //    Console.WriteLine(names);
+            //}
+
+            //foreach(var numbers in number)
+            //{
+            //    Console.WriteLine(numbers);
+            //}
+
+            //var number = 0;
+            //while (number < 10)
+            //{
+            //   Console.WriteLine(number);
+            //   number++;
+            //}
+
+
+        }
+    }
+}
