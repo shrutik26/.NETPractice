36c78d5aae49338a45ba2659a3d87c4276e36b5c
LZ3OBZ@alsn.com <MY_NAME@example.com>
2022-01-05 16:02:33 +0530

    Initial Commit

---
+++ MethodOverriding/MethodOverriding/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MethodOverriding
+{
+    class baseClass
+    {
+        public void Greetings()    
+        {
+            Console.WriteLine("baseClass Saying Hello!");
+        }
+    }
+    class derivedClass : baseClass
+    {
+        public  void Greetings()
+        {
+            
+            Console.WriteLine("subClass Saying Hello!");
+            //base.Greetings();
+        }
+        public void Greetings1()
+        {
+            Console.WriteLine("i am in Greetings1");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            derivedClass obj1 = new derivedClass();
+            //baseClass obj1 = new baseClass();
+            obj1.Greetings();
+            obj1.Greetings1();
+            Console.ReadLine();
+        }
+    }
+}
