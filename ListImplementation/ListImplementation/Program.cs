4bf6a3a139be90aa8a2550b6c585d4d66d336e9c
LZ3OBZ@alsn.com <MY_NAME@example.com>
2022-01-05 16:01:53 +0530

    Initial Commit

---
+++ ListImplementation/ListImplementation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4, 5 };
+
+            numbers.Add(6);
+            numbers.AddRange(new int[] { 7, 8, 9 });
+
+            foreach(var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine("IndexOf 6 :" + numbers.IndexOf(6));
+            Console.WriteLine("LastIndex of 6 :" + numbers.LastIndexOf(6));
+            Console.WriteLine("Count of list:" + numbers.Count);
+            numbers.Remove(3);
+
+            foreach(var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+   
+        }
+    }
+}
