c385e1041e5106c20842e1c0a8901b98041968f1
LZ3OBZ@alsn.com <MY_NAME@example.com>
2022-01-05 15:57:01 +0530

    Initial Commit

---
+++ Array/Array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ArrayImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 12, 4, 5, 2, 18 };
+            Console.WriteLine("Length :" + numbers.Length);
+
+            var number = Array.IndexOf(numbers, 5);
+            Console.WriteLine("Index Of 5 :" + number);
+
+            Array.Clear(numbers, 0, 2);
+            Console.WriteLine("Effect of clear :");
+            foreach(var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            Array.Sort(numbers);
+            Console.WriteLine("Effect of Sorting");
+            foreach(var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            Array.Reverse(numbers);
+            Console.WriteLine("Effect of Revering");
+            foreach(var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+    }
+}
